3bf56962af8cb395af4b176e268e8be190460e0d
NimnaKs <nimnakaveesha123456@gmail.com>
2024-06-25 11:21:19 +0530

    Init Authenticate commit done

---
+++ AuthenticateService/src/test/java/lk/ijse/AuthenticateService/AuthenticateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.AuthenticateService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
